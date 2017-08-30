73caa7d5f906de80052c8f52dcbb6d720ebd6d5d
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-08-30 09:34:49 -0700

    in class

---
+++ PWM+Serial/PWM+Serial.ino
@@ -0,0 +1,37 @@
+
+String inData;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+  // reserve 200 bytes for the inputString:
+}
+
+void loop() {
+    while (Serial.available() > 0)
+    {
+        char recieved = Serial.read();
+        inData += recieved; 
+
+        // Process message when new line character is recieved
+        if (recieved == '\n')
+        {
+            Serial.print("Arduino Received: ");
+            Serial.print(inData);
+
+            int x = inData.toInt();
+            if(x == 1){
+               digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+            }
+            else if (x == 0){
+               digitalWrite(LED_BUILTIN, LOW);   // turn the LED off
+            }
+
+            inData = ""; // Clear recieved buffer
+        }
+    }
+}
+
+
