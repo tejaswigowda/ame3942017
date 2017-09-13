632736de962fbad0da7f231f45bd459b721fc24a
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-09-13 10:18:45 -0700

    in class

--- evalServer/server.js
+++ evalServer/server.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 var callback = function (req, res) { // req -> request object; res -> response object
   var eq = req.url.replace("/","");;
-  var r = eq + " = " + eval(eq).toString() + "\n";
+  var r = eq + " = " + eval(eq) + "\n";
    res.writeHead(200, {'Content-Type': 'text/plain'}); // send response header
    res.end(r); // send response body
 }
