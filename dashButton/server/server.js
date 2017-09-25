41ab450d2ece31053dd3e8a20d0560fed1358f5b
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-09-25 09:42:37 -0700

    ac

--- dashButton/server/server.js
+++ dashButton/server/server.js
@@ -6,5 +6,5 @@ var callback = function (req, res) { // req -> request object; res -> response o
 }
 
 var server = http.createServer(callback) // create an http server
-server.listen(1234, process.env.HOSTNAME); // make server listen to port 1234
+server.listen(1234, process.env.HOSTNAME || "localhost"); // make server listen to port 1234
 console.log("Server running at: "+ "http://127.0.0.1:1234");
